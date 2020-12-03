c68c07b00692ebe92953215a0330a64dcc61a65a
Łukasz Pięta <pieta.lukasz98@gmail.com>
2020-12-03 19:45:58 +0100

    Closes window

--- server.R
+++ server.R
@@ -1,14 +1,131 @@
 library(shiny)
+library(readxl)
+library(summarytools)
+library(gtsummary)
+library(tidyverse)
+library(dplyr)
+library(knitr)
+library(DT)
+library(ggplot2)
+library(reshape2)
+library(gganimate)
+library(hrbrthemes)
+library( rgl )
+library(magick)
+library(tibble)
+library(caret)
+
+df <- read_excel("wuhan_blood_sample_data_Jan_Feb_2020.xlsx")
+
+patients_df <- df %>% group_by(`Admission time`, `Discharge time`, gender, age, outcome) %>%
+  summarise(PATIENT_ID = sum(PATIENT_ID, na.rm = TRUE), `Total records` = n()) 
+
+patients_df <- patients_df %>%
+  mutate(`Days in hospital` = ceiling(difftime(`Discharge time`, `Admission time`, units = "days")))
+
+df <- full_join(patients_df %>% ungroup() %>% select(`Admission time`, PATIENT_ID), df %>% select(-PATIENT_ID), by="Admission time")
+
+df$gender<-ifelse(df$gender==1, 'Male', 'Female') 
+df <- df %>% mutate(gender = as.factor(gender))
+
+patients_df$gender<-ifelse(patients_df$gender==1, 'Male', 'Female') 
+patients_df <- patients_df %>% mutate(gender = as.factor(gender))
+
+df$outcome<-ifelse(df$outcome==1, 'Death', 'Survival')
+df <- df %>% mutate(outcome = as.factor(outcome))
+
+patients_df$outcome<-ifelse(patients_df$outcome==1, 'Death', 'Survival')
+patients_df <- patients_df %>% mutate(outcome = as.factor(outcome))
+
+patients_df <- df %>% group_by(PATIENT_ID, outcome, `Admission time`, `Discharge time`, gender, age) %>% summarise(missing_test=is.na(RE_DATE)) %>%
+  group_by(PATIENT_ID, outcome, `Admission time`, `Discharge time`, gender, age) %>%
+  summarise(test_provided = ifelse(missing_test, 0, 1)) %>%
+  group_by(PATIENT_ID, outcome, `Admission time`, `Discharge time`, gender, age) %>%
+  summarise(`Total blood tests`=sum(test_provided)) %>%
+  mutate(`Days in hospital` = ceiling(difftime(`Discharge time`, `Admission time`, units = "days")))
+
+last_test_df <- df %>% 
+  group_by(PATIENT_ID, outcome, gender, age) %>% 
+  fill_(names(df)) %>% 
+  fill_(names(df), "up") %>% 
+  summarise_at(vars(`Hypersensitive cardiac troponinI`:creatinine), function(x) last(x,order_by = is.na(x)))
+
+
+cleaned_df <- last_test_df %>% ungroup() %>% select(outcome, `neutrophils(%)`, `Lactate dehydrogenase`, `High sensitivity C-reactive protein`) %>% filter_all(function(x) !is.na(x))  %>%
+  rename(CRP=`High sensitivity C-reactive protein`, LDH=`Lactate dehydrogenase`, NEUTR= `neutrophils(%)`)
+
+set.seed(23)
+inTraining <- 
+  createDataPartition(
+    y = cleaned_df$outcome,
+    p = .70,
+    list = FALSE)
+
+
+training <- cleaned_df[ inTraining,]
+testing  <- cleaned_df[-inTraining,]
+
+rfGrid <- expand.grid(mtry = 10:30)
+ctrl <- trainControl(
+  method = "repeatedcv",
+  classProbs = TRUE,
+  number = 2,
+  repeats = 5)
+
+set.seed(23)
+fit <- train(outcome ~ .,
+             data = training,
+             method = "rf",
+             metric = "ROC",
+             preProc = c("center", "scale"),
+             trControl = ctrl,
+             tuneGrid = rfGrid,
+             ntree = 30)
+rfClasses <- predict(fit, newdata = testing)
+confusionMatrix(data = rfClasses, testing$outcome)
+
+
 shinyServer(
   function(input, output) {
       
-      output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      output$prediction <- renderText({
+        CRP <- input$CRP
+        LDH <-input$LDH
+        NEUTR <- input$NEUTR
+        new_data <- data.frame(NEUTR, LDH, CRP)
+      
+        paste("Percentage chances of patient's survival:  ", predict(fit, newdata=new_data,  type = "prob")$Survival * 100)
         
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        }) 
+      
+      output$webGL <- renderRglwidget({
+        try(rgl.close())
+        bg3d(color = "white")
+        CRP <- input$CRP
+        LDH <-input$LDH
+        NEUTR <- input$NEUTR
+        color <- 'yellow'
+        new_data <- data.frame(NEUTR, LDH, CRP)
+        outcome <- predict(fit, newdata=new_data)
+        size <- 16
+        new_data <- data.frame(outcome, NEUTR, LDH, CRP, color, size)
+        
+        corr_visualize_df  <- cleaned_df
+        mycolors <- c('royalblue1', 'darkcyan')
+        corr_visualize_df$color <- mycolors[ as.numeric(corr_visualize_df$outcome) ]
+        corr_visualize_df$size <- 6
+        corr_visualize_df <- rbind(corr_visualize_df, new_data)
+        mycolors <- c('royalblue1', 'darkcyan', 'yellow')
+        par(mar=c(0,0,0,0))
+        plot3d( 
+          x=corr_visualize_df$CRP, y=corr_visualize_df$LDH, z=corr_visualize_df$NEUTR, 
+          col = corr_visualize_df$color, 
+          type = 's', 
+          radius = corr_visualize_df$size,
+          legend=TRUE,
+          xlab="CRP", ylab="LDH", zlab="Neutr")
+        rglwidget()
+        rgl.close()
       })
   }
 )
--- ui.R
+++ ui.R
@@ -1,23 +1,28 @@
 library(shiny)
+library(shinyRGL)
 shinyUI(
   fluidPage(
     
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("COVID-19 analysis"),
     
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
+      
       sidebarPanel(
-        sliderInput("bins",
-                    "Number of bins:",
-                    min = 1,
-                    max = 50,
-                    value = 30)
+        h3("Enter data of new patient:"),
+        sliderInput("LDH", "LDH U/l", min=100, max=2000, value=200),
+        sliderInput("CRP", "High sensitivity C-reactive protein", min=0, max=350, value=100),
+        sliderInput("NEUTR", "Neutrophils (%)", min=0, max=100, value=50)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        plotOutput("distPlot")
+        textOutput("prediction"),
+        
+        #htmlOutput("plot")
+        #webGLOutput("webGL")
+        rglwidgetOutput("webGL", width = 600, height = 600)
       )
     )
   )
